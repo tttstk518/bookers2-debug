b1aaac4f9913d1663c2e9f1eba82b5ac1b096cb2
tttstk518 <tttstk518@gmail.com>
2022-12-04 14:45:34 +0000

    応用課題2-3

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,17 @@
+class FavoritesController < ApplicationController
+
+  def create
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: book.id)
+    favorite.save
+    redirect_to user_path(current_user)
+  end
+
+  def destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    redirect_to user_path(current_user)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root :to => 'homes#top'
   devise_for :users
   resources :users,only: [:show, :edit, :update, :index]
-  resources :books
+  resources :books do
+     resource :favorites, only: [:create, :destroy]
+     resources :book_comments, only: [:create]
+   end
   get 'home/about' => 'homes#about', as: 'about'
+
 end
